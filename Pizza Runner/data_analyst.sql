6af3f8d386e4a0d06e5faa35943d6b54554cb93b
louisvigo <louisvigo219@gmail.com>
2024-05-20 22:57:35 +0800

    Update Project

--- Pizza Runner/data_analyst.sql
+++ Pizza Runner/data_analyst.sql
@@ -24,3 +24,38 @@ WHERE
         runner_orders.cancellation IS NULL
 GROUP BY runner_orders.runner_id
 ORDER BY runner_orders.runner_id ASC;
+
+-- How many of each type of pizza was delivered?
+SELECT 
+       pizza_names.pizza_name,
+       COUNT(customer_orders.customer_id) AS delivered
+FROM pizza_runner.customer_orders AS customer_orders
+LEFT JOIN pizza_runner.runner_orders AS runner_orders ON runner_orders.order_id = customer_orders.order_id
+LEFT JOIN pizza_runner.pizza_names AS pizza_names ON pizza_names.pizza_id = customer_orders.pizza_id
+WHERE
+        runner_orders.cancellation NOT LIKE '%Cancellation%' OR
+        runner_orders.cancellation IS NULL
+GROUP BY pizza_name
+ORDER BY delivered DESC;
+
+-- How many Vegetarian and Meatlovers were ordered by each customer?
+SELECT 
+        customer_orders.customer_id,
+        COUNT(CASE WHEN pizza_names.pizza_name = 'Meatlovers' THEN 1 END) AS meat_lover,
+        COUNT(CASE WHEN pizza_names.pizza_name = 'Vegetarian' THEN 1 END) AS vegetarian
+FROM pizza_runner.customer_orders AS customer_orders
+LEFT JOIN pizza_runner.runner_orders AS runner_orders ON runner_orders.order_id = customer_orders.order_id
+LEFT JOIN pizza_runner.pizza_names AS pizza_names ON pizza_names.pizza_id = customer_orders.pizza_id
+GROUP BY customer_orders.customer_id;
+
+-- What was the maximum number of pizzas delivered in a single order?
+SELECT
+        customer_orders.order_time,
+        COUNT(customer_orders.order_id) AS number_of_order
+FROM pizza_runner.customer_orders AS customer_orders
+LEFT JOIN pizza_runner.runner_orders AS runner_orders ON runner_orders.order_id = customer_orders.order_id
+WHERE 
+        runner_orders.cancellation NOT LIKE '%Cancellation%' OR
+        runner_orders.cancellation IS NULL
+GROUP BY customer_orders.order_time
+ORDER BY COUNT(customer_orders.order_id) DESC;
